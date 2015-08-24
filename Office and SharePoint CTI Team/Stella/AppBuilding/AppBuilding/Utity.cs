f6f9251eaaf070b7dfe6d22b9b226ef13bda8786
StellaFa <v-stfa@microsoft.com>
2015-08-24 02:59:34 -0700

    Update collect data code
    Update the method about fTest Query

--- Office and SharePoint CTI Team/Stella/AppBuilding/AppBuilding/Utity.cs
+++ Office and SharePoint CTI Team/Stella/AppBuilding/AppBuilding/Utity.cs
@@ -16,6 +16,10 @@ namespace AppBuilding
 
             Console.ReadKey();
           }
+
+        /// <summary>
+        /// Connect Maddog Database
+        /// </summary>
         public static void connectDB()
         {
             MadDogObjects.Utilities.Security.SetDB("mdsql3", "OrcasTS");
@@ -23,12 +27,13 @@ namespace AppBuilding
             MadDogObjects.Utilities.Security.AppOwner = Environment.UserName; 
         }
 
-        static List<RunInfo> GetAutomationList()
+        public static List<RunInfo> GetAutomationList()
         {
             Console.WriteLine("Please enter a Run ID:");
             string RunID = Console.ReadLine();
             int runid = int.Parse(RunID);
             Run run = new Run(runid);
+            
             PassRate PR = new PassRate(runid);
 
             List<RunInfo> runlist = new List<RunInfo>();
@@ -38,9 +43,10 @@ namespace AppBuilding
                 IntialPR = PR.InitialPassingRate.ToString(),
                 FinalPR = PR.FinalPassingRate.ToString(),
                 RunID = runid.ToString(),
-
                 OS = run.OS.ToString(),
-                TestQuery = run.TestcaseQuery.ToString()
+                OSLan = run.OS.LanguageLocale.ToString(),
+                VSLan = run.Product.LanguageLocale.ToString(),
+                TestQuery = run.TestcaseQuery.ID.ToString(),
             };
 
                
@@ -48,9 +54,12 @@ namespace AppBuilding
                 return runlist;
             }
 
-  
+         /// <summary>
+         /// Create a automation xml
+         /// </summary>
+         /// <param name="automation"></param>
 
-         static  void CreateAutomationXML(String automation)
+        public static  void CreateAutomationXML(String automation)
         {
             StringBuilder xmlResult = new StringBuilder("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
             List<RunInfo> AutomationList = GetAutomationList();
@@ -65,8 +74,8 @@ namespace AppBuilding
                     xmlResult.AppendFormat("<FinalPR>{0}</FinalPR>", RunInfo.FinalPR);
                     xmlResult.AppendFormat("<RunID>{0}</RunID>", RunInfo.RunID);
                     xmlResult.AppendFormat("<OS>{0}</OS>", RunInfo.OS);
-                    //xmlResult.AppendFormat("<OSLan>{0}</OSLan>", RunInfo.OSLan);
-                   // xmlResult.AppendFormat("<VSLan>{0}</VSLan>", RunInfo.VSLan);
+                    xmlResult.AppendFormat("<OSLan>{0}</OSLan>", RunInfo.OSLan);
+                    xmlResult.AppendFormat("<VSLan>{0}</VSLan>", RunInfo.VSLan);
                     xmlResult.AppendFormat("<TestQuery>{0}</TestQuery>", RunInfo.TestQuery);
                 }
                 xmlResult.Append("</Run Info>");
@@ -78,7 +87,7 @@ namespace AppBuilding
                 FileStream fileStream = new FileStream(automation, FileMode.Create);
                 StreamWriter streamWriter = new StreamWriter(fileStream);
                 streamWriter.WriteLine(xmlResult);
-                Console.WriteLine("Create successfully!");
+                Console.WriteLine("Create successfully! You can gain detail in Building/AppBuilding/bin/Debug/automation.xml");
                 streamWriter.Close();
                 fileStream.Close();
             }
