6289b700e598ce979b9ead1f6c60e5234d3e97b9
Stella Fan <v-stfa@microsoft.com>
2015-09-13 19:51:06 -0700

    Update project name
    Update project name and namespace

---
+++ Office and SharePoint CTI Team/Stella/CollectData/CollectData/RunInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectData
+{
+    public class RunInfo
+    {
+        public string BuildNo { set; get; }
+        public string IntialPR { set; get; }
+        public string FinalPR { set; get; }
+        public string RunID { set; get; }
+        public string OS { set; get; }
+        public string OSLan { set; get; }
+        public string VSLan { set; get; }
+        public string TestQuery { set; get; }
+    }
+}
