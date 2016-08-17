c78a5cc67e519c0fd809333a94a462dc5bec7249
Incyuna2z <damn_frank@163.com>
2016-08-17 23:03:27 +0800

    DemoToIntroduceUnitTest
    1. Why we need Unit test in agile testing development
    2. Unit test can't do anything to void the fault of code design

---
+++ test/UnitTesting/UnitTesting/DeviceClassV1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class DeviceClassV1
+    {
+        public string name = "";
+        public string status = "";
+
+        public DeviceClassV1(string Name)
+        {
+            name = Name;
+        }
+
+        public void set_status(string Status)
+        {
+            status = Status;
+        }
+
+        public string read_status()
+        {
+            return status;
+        }
+    }
+
+    public class testV1
+    {
+        [Fact]
+        public void testingMethod1()
+        {
+            DeviceClassV1 testClassV1 = new DeviceClassV1("kitchen light");
+            testClassV1.set_status("on");
+            Assert.Equal("on", testClassV1.read_status());
+            //Assert.Equal("off", status);
+
+            /*
+            1. The specification of the variable status say that it should display the current switching state of a device using the values "On" or "Off",
+            but in our case, status can be allocated to any other value, such as "30%"
+            2. Once this check has been buit in, we also have to make sure that status can not be manipulated as well. 
+            */
+        }
+    }
+
+}
---
+++ test/UnitTesting/UnitTesting/DeviceClassV2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    class DeviceClassV2
+    {
+        private string name = "";
+        private string status = "";
+
+        public DeviceClassV2(string Name)
+        {
+            name = Name;
+            status = String.Empty;
+        }
+
+        public void set_status(string Status)
+        {
+            if (IsStatusValid(Status))
+            {
+                status = Status;
+            }
+            else
+            {
+                Exception e = new Exception("You enter a invalid value, Status just could be 'On' or 'Off'");
+                throw e;
+            }                     
+        }
+
+        public string read_status()
+        {
+            return status;
+        }
+
+        public string read_name()
+        {
+            return status;
+        }
+
+        public bool IsStatusValid(string Status)
+        {
+            if(Status == "On" || Status == "Off")
+                return true;
+            else
+            return false;
+        }
+    }
+    
+    public class testV2
+    {
+        [Fact]
+        public void testingMethod1()
+        {
+            DeviceClassV2 testClassV2 = new DeviceClassV2("Bedroom light");
+            testClassV2.set_status("On");
+            Assert.Equal("On", testClassV2.read_status());
+        }
+    }
+}
