1b9fc4144a72074ba2e8a62aa92fe6cc60a6d0a7
Incyuna2z <damn_frank@163.com>
2016-09-07 23:06:11 +0800

    WPF sample in Azure SQL

---
+++ test/EF/UsingEFInAzureSQL/UsingEFInAzureSQL/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿1. Please provide the password in app.config both of two projects
+2.In this demo project, the csdl ssdl and msl file is putted under C:\Users\Lenovo\Documents\Visual Studio 2015\Projects\UsingEFInAzureSQL. Please build UsingEFInAzureSql project 
+and replace these files to your peferred file location. Update app.config accordingly
---
+++ test/EF/UsingEFInAzureSQL/WPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UsingEFInAzureSQL;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BreakAwayEntities _context;
+        private List<Activity> _activity;
+        private List<Location> _location;
+        private List<Lodging> _lodging;
+        private List<Event> _event;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _context = new BreakAwayEntities();
+            _activity = _context.Activities.OrderBy(a => a.ActivityID).ToList();
+            _location = _context.Locations.OrderBy(lo => lo.LocationName).ToList();
+            _lodging = _context.Lodgings.OrderBy(l => l.LodgingName).ToList();
+            _event = _context.Events.OrderBy(ev => ev.EventID).ToList();
+
+            System.Windows.Data.CollectionViewSource locationViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("locationViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // locationViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource lodgingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("lodgingViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // lodgingViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource eventViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("eventViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // eventViewSource.Source = [generic data source]
+            locationViewSource.Source = _location;
+            lodgingViewSource.Source = _lodging;
+            eventViewSource.Source = _event;
+        
+
+
+        }
+    }
+}
