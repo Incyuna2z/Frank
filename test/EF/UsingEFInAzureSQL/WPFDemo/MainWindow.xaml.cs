ab89c39e31c589e70ae7d445ba14574190cd29d8
Incyuna2z <damn_frank@163.com>
2016-09-08 22:55:19 +0800

    Update MeteDataArtifactProcessing and new functionality

--- test/EF/UsingEFInAzureSQL/WPFDemo/MainWindow.xaml.cs
+++ test/EF/UsingEFInAzureSQL/WPFDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,10 +36,14 @@ namespace WPFDemo
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _context = new BreakAwayEntities();
-            _activity = _context.Activities.OrderBy(a => a.ActivityID).ToList();
+            //I don't update property, so activity means the name of activity here
+            _activity = _context.Activities.OrderBy(a => a.Activity1).ToList();
             _location = _context.Locations.OrderBy(lo => lo.LocationName).ToList();
             _lodging = _context.Lodgings.OrderBy(l => l.LodgingName).ToList();
-            _event = _context.Events.OrderBy(ev => ev.EventID).ToList();
+            //event and activity has a many-to-many relationship
+            _event = _context.Events.Include("Activities").OrderBy(ev => ev.EventID).ToList();
+
+            activityComboBox.ItemsSource = _activity;
 
             System.Windows.Data.CollectionViewSource locationViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("locationViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
@@ -52,9 +57,35 @@ namespace WPFDemo
             locationViewSource.Source = _location;
             lodgingViewSource.Source = _lodging;
             eventViewSource.Source = _event;
-        
+        }
 
+        //Save changes
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //The BindingSource coodinate user action with entities.If a user edited data on the form,the bindingsource
+            //automatically pushed that change into the entity, even the related entities
+            _context.SaveChanges();
+        }
 
+        private void locationComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            eventListBox.Items.SortDescriptions.Add(new SortDescription("Location.LocationName", ListSortDirection.Ascending));
+            eventListBox.Items.SortDescriptions.Add(new SortDescription("StartDate", ListSortDirection.Descending));
+        }
+
+        //Add new event
+        private void btnAddActivity_Click(object sender, RoutedEventArgs e)
+        {
+            Activity addedActivity = activityComboBox.SelectedItem as Activity;
+
+            if(addedActivity!=null)
+            {
+                var addedEvent = eventListBox.SelectedItem as Event;
+                if(addedEvent!=null)
+                {
+                    addedEvent.Activities.Add(addedActivity);
+                }
+            }
         }
     }
 }
